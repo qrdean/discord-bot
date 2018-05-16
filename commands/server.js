44321bd25dca1d6116c0f16561f81110486915e8
ADJACENT\QDean <qdean@adjacent-tech.com>
2018-05-16 11:33:35 -0500

    adding prettier to project files and editerconfig

--- commands/server.js
+++ commands/server.js
@@ -1,7 +1,11 @@
 module.exports = {
-    name: 'server',
-    description: 'Server!',
-    execute(message) {
-        message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`)
-    },
+  name: 'server',
+  description: 'Server!',
+  execute(message) {
+    message.channel.send(
+      `This server's name is: ${message.guild.name}\nTotal members: ${
+        message.guild.memberCount
+      }`
+    )
+  }
 }
--- index.js
+++ index.js
@@ -8,33 +8,31 @@ client.commands = new Discord.Collection()
 const commandFiles = fs.readdirSync('./commands')
 
 commandFiles.map(file => {
-    const command = require(`./commands/${file}`)
+  const command = require(`./commands/${file}`)
 
-    // set a new item in the Collection
-    // with the key as the command name and the value as the exported module
-    client.commands.set(command.name, command)
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.name, command)
 })
 
-
 client.on('ready', () => {
-    console.log('Ready!')
+  console.log('Ready!')
 })
 
 client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return
+  if (!message.content.startsWith(prefix) || message.author.bot) return
 
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const command = args.shift().toLowerCase()
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
 
-    if (!client.commands.has(command)) return
+  if (!client.commands.has(command)) return
 
-    try {
-        client.commands.get(command).execute(message, args)
-    }
-    catch (error) {
-        console.error(error)
-        message.reply('there was an error trying to execture that command!')
-    }
+  try {
+    client.commands.get(command).execute(message, args)
+  } catch (error) {
+    console.error(error)
+    message.reply('there was an error trying to execture that command!')
+  }
 })
 
 client.login(token)
